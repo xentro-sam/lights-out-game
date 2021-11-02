96e8bd1658542af1ac09a5359837dff250e14c94
Shivam Agarwal <itisshivam22@gmail.com>
2021-11-02 20:51:12 +0530

    Updates

---
+++ precache-manifest.805c7ce74440a417cf869b88fee200ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2979eeede58dbae5e524",
+    "url": "/lights-out-game/static/css/main.b2c92b0a.chunk.css"
+  },
+  {
+    "revision": "2979eeede58dbae5e524",
+    "url": "/lights-out-game/static/js/main.2979eeed.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "74c54e9346110292ea83b00f8114af8c",
+    "url": "/lights-out-game/index.html"
+  }
+];
